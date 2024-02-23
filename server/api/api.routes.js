1a5c412102b2b4a2193ec7ebe3a9b31900f94876
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-02-23 11:37:00 -0700

    working on server bug

--- server/api/api.routes.js
+++ server/api/api.routes.js
@@ -3,10 +3,10 @@ const express = require('express')
 const apiRouter = express.Router()
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, ".../client/dist")));
+app.use(express.static(path.resolve(__dirname, "./../client/dist")));
 
 apiRouter.get('/',(req, res) => {
-    const file = __dirname + '.../client/index.html'
+    const file = path.join(__dirname, "../../client/index.html");
     res.send(file);
 })
 
