78330fc7ab8fdeb4af6395792171516e3d6965fd
AndersonBillss <anderson@thebillsfamily.com>
2024-02-27 09:49:49 -0700

    created folder for passport middleware

---
+++ server/auth/secure-login.js
@@ -0,0 +1,24 @@
+//imported modules
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+const login = require("connect-ensure-login");
+const bcrypt = require("bcryptjs");
+
+function secureLogIn(){
+    
+//middleware configuration
+app.use(
+    session({
+      secret: "asdfasdf",
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+
+console.log('log in')
+}
+module.exports=secureLogIn
+
