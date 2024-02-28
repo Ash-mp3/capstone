2b1f6ec1cad7dd7f5634cbadde25da3a53b448ac
AndersonBillss <anderson@thebillsfamily.com>
2024-02-28 10:25:18 -0700

    display courses from api

--- client/src/components/AccordionCom.jsx
+++ client/src/components/AccordionCom.jsx
@@ -6,7 +6,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 
-export default function AccordionCom() {
+export default function AccordionCom(title, description) {
   return (
       <Accordion>
         <AccordionSummary
@@ -14,10 +14,10 @@ export default function AccordionCom() {
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          Introduction to Computer Science
+          {title}
         </AccordionSummary>
         <AccordionDetails>
-        "This course will introduce students to the fundamental concepts behind computers and computer programming. Topics covered include basic programming logic, algorithm development, computer architecture, and software engineering."
+          {description}
         </AccordionDetails>
         <AccordionActions>
           <Button id='addCourse'>Add Course</Button>
