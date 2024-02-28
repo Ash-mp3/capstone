e7bb50a54ddb135b7037003fbcab549e8c91d8fb
AndersonBillss <anderson@thebillsfamily.com>
2024-02-28 10:25:18 -0700

    display courses from api

--- client/src/components/courses.jsx
+++ client/src/components/courses.jsx
@@ -37,6 +37,10 @@ function Courses() {
     });
   },[])
 
+/*   if(courses !== ''){
+    console.log(courses)
+  }
+ */
   return (
     <div className="Courses">
     {
@@ -46,29 +50,21 @@ function Courses() {
         </ResponsiveAppBar>
       
         <div id="listOfAvailable">
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-
-          <AccordionCom>
-          </AccordionCom>
-          
-          <AccordionCom>
-          </AccordionCom>
+          { 
+            courses !== '' 
+            ?
+            courses.map((course, index) => {
+              return(
+                <div key={index}>
+                  {AccordionCom(course.title, course.description)}
+                </div>
+              )
+            })
+            : 
+            <div>
+              loading...
+            </div>
+          }
         </div>
 
         <Footer>
