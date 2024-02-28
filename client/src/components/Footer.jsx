46a974c51d955b7ce1e054b1cc3726daeb07aa45
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2024-02-28 08:59:27 -0700

    added components like a navbar and footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer id="typicalFooter" className="w-full h-[25px] flex justify-center items-center absolute bottom-0">
+        <p>Help</p>
+        <p>@2023 - MtecPro</p>
+        <p>About</p>
+    </footer>
+    );
+}
+export default Footer;
+
+
+
