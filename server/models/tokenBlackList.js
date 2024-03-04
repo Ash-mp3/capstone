51607330b4e2cba697c869de2c349bf605fb26b0
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-03-04 08:13:25 -0700

    working on auth in the db

--- server/models/tokenBlackList.js
+++ server/models/tokenBlackList.js
@@ -1,7 +1,12 @@
 const client = require("../config/database");
 
-const sendToken = async () => {
+const sendToken = async (token, tokenExp) => {
     const sendTokenQuery = `
-        
-    `
+        INSERT INTO token_blacklist (token, tokenExp)
+        VALUES (${token}, ${tokenExp})
+    `;
+
+    
 }
+
+module.exports = { sendToken }
