a76e4b73f197a17ceebd57c22583d358672f21ea
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-03-05 15:55:47 -0700

    login page now validates salted passwords for the database, allowing users to login

--- server/api/api.routes.js
+++ server/api/api.routes.js
@@ -5,47 +5,48 @@ const { expressjwt } = require("express-jwt");
 
 //middleware
 const secureLogIn = require("../middleware/secure-login.js");
-const logout = require("../middleware/logout.js")
-const signup = require("../middleware/signup.js")
+const logout = require("../middleware/logout.js");
+const signup = require("../middleware/signup.js");
 
 //controllers
-const getCourses = require("../controllers/courseController.js")
+const getCourses = require("../controllers/courseController.js");
 
 //Environment variables
 const secret = process.env.JWT_SECRET;
 
-
-
 //responds with a jwt and a logged in status
 apiRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  try {
-    const result = await secureLogIn(email, password)
-    res.status(result.status).json(result.res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({mesage: "ERror"})
-  }
-})
+  const result = await secureLogIn(email, password);
+  res.status(result.status).json(result.res);
+});
 
 //responds with a logged out status
-apiRouter.post("/logout", expressjwt({ secret: secret, algorithms: ["HS256"] }), async(req, res) => {
-  const authorization = req.headers.authorization
-  const result = logout(authorization)
-  res.status(result.status).json(result.res);
-})
+apiRouter.post(
+  "/logout",
+  expressjwt({ secret: secret, algorithms: ["HS256"] }),
+  async (req, res) => {
+    const authorization = req.headers.authorization;
+    const result = logout(authorization);
+    res.status(result.status).json(result.res);
+  }
+);
 
 //send user sign in info to database
 apiRouter.post("/signup", async (req, res) => {
-  const info = req.body
-  const result = await signup(info)
-  res.status(result.status).json(result.res)
+  const info = req.body;
+  const result = await signup(info);
+  res.status(result.status).json(result.res);
 });
 
 //get courses from database
-apiRouter.get("/courses", expressjwt({ secret: secret, algorithms: ["HS256"] }), async (req, res) => {
-  const result = await getCourses()
-  res.status(result.status).json(JSON.parse(result.res))
-});
+apiRouter.get(
+  "/courses",
+  expressjwt({ secret: secret, algorithms: ["HS256"] }),
+  async (req, res) => {
+    const result = await getCourses();
+    res.status(result.status).json(JSON.parse(result.res));
+  }
+);
 
-module.exports = apiRouter
+module.exports = apiRouter;
--- server/models/loginUser.js
+++ server/models/loginUser.js
@@ -1,11 +1,8 @@
 const client = require("../config/database")
 
 const loginUser = async (email, password) => {
-    const loginQuery = `
-        select * from users where email = '${email}'
-    `
+    const loginQuery = `select * from users where email = '${email}'`
     const login = await client.query(loginQuery);
-   
     if (login.rowCount === 1) {
         return `${login.rows[0].password}`
     } else {
