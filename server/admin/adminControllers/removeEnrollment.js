b555a5f201f3272202b3b483bc325e0d2e421cb9
AndersonBillss <anderson@thebillsfamily.com>
2024-03-13 14:15:15 -0600

    created functionality for admins editing user courses

--- server/admin/adminControllers/removeEnrollment.js
+++ server/admin/adminControllers/removeEnrollment.js
@@ -1,7 +1,12 @@
 const client = require("../../config/database")
 
 async function removeEnrollment(user_id, class_id){
-
+    const removeEnrollmentQuery = `
+    delete from enrollments 
+    where user_id = ${user_id} and 
+    class_id = ${class_id}`
+    client.query(removeEnrollmentQuery)
+    return({status: 201, msg: "Successfully removed user"})
 }
 
 module.exports = removeEnrollment
